2238195dcd5ded747b71430db102d5672570a94e
Tiepnnk <jurgenklopp047@gmail.com>
2026-08-10 13:31:00 +0700

    feat(auth): update validate password.

--- src/main/java/com/example/vex360/features/auth/dtos/request/RegisterRequest.java
+++ src/main/java/com/example/vex360/features/auth/dtos/request/RegisterRequest.java
@@ -19,6 +19,8 @@ public class RegisterRequest {
 
     @NotBlank(message = "Mật khẩu không được để trống")
     @Size(min = 8, max = 72, message = "Mật khẩu phải có từ 8 đến 72 ký tự.")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*]).*$",
+            message = "Mật khẩu phải chứa ít nhất 1 chữ hoa, 1 số và 1 ký tự đặc biệt (!@#$%^&*).")
     private String password;
 
     @NotBlank(message = "Họ và tên không được để trống")
